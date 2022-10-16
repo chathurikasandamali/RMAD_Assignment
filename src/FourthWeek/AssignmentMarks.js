dc59865e85d325e0921921e5aca3dfe967102502
haCSandamali <chathurikasanda@gmail.com>
2022-10-16 11:29:00 +0530

    switch case

--- src/FourthWeek/AssignmentMarks.js
+++ src/FourthWeek/AssignmentMarks.js
@@ -8,7 +8,7 @@ const AssignmentMarks = () => {
   }, []);
 
   const functionOne = () => {
-    const marks = 80;
+    const marks = 70;
 
     //If condition
     // if (marks >= 75) {
@@ -24,11 +24,24 @@ const AssignmentMarks = () => {
     // }
 
     //Ternary Operator
-    marks >= 75 ? console.log('Result: A ')
-    : marks >= 65 ? console.log('Result: B ')
-    : marks >= 55 ? console.log('Result: C ')
-    : marks >= 35 ? console.log('Result: S ')
-    : console.log('Result: Fail ');
+    // marks >= 75 ? console.log('Result: A ')
+    // : marks >= 65 ? console.log('Result: B ')
+    // : marks >= 55 ? console.log('Result: C ')
+    // : marks >= 35 ? console.log('Result: S ')
+    // : console.log('Result: Fail ');
+
+    //Switch case
+    switch (true) {
+        case marks >= 75:
+            console.log('Result: A ');
+            break;
+        case marks >= 65:
+            console.log('Result: B ');
+            break;
+        default:
+            console.log('Result: Fail ');
+            break;
+    }
   };
 
   return (
